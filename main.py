3ee6ded9757d4d712b6776f3a35d9625624ae027
jackr <jackryder1@live.com.au>
2022-08-17 17:21:42 +1000

    V0.01

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import ctypes
 
 myappid = 'URImager' # arbitrary string
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
-
+#comment
 customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
